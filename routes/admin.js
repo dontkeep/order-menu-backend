24f10e9ea47c33b68d742967ac91468d4a91f6c3
dontkeep <alsdiaryy@gmail.com>
2025-03-19 18:15:06 +0700

    idk what changed

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 // Admin-specific route
 router.get('/admin-data', verifyToken, checkRole(0), (req, res, next) => {
+  const { user } = req;
+  if (!user) return res.status(401).send('Unauthorized');
+  
   // Admin-specific logic here
   res.send('This is admin data');
 });
